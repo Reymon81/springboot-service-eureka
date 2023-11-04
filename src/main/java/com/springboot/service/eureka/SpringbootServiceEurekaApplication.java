49f2de24cfe8eafd974927e3f3e7eb9b4b6c9485
Reymon81 <ramon.torres.81@gmail.com>
2023-11-05 00:03:34 +0100

    Initial commit

---
+++ src/main/java/com/springboot/service/eureka/SpringbootServiceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.service.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/service/eureka/SpringbootServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.service.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
